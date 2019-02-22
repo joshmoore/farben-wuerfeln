dcbd11606713e3d7a33b705988e2ea8ae470b3d3
jmoore <josh@glencoesoftware.com>
2019-02-22 11:49:05 +0100

    add style

--- wuerfeln.js
+++ wuerfeln.js
@@ -1,3 +1,17 @@
-$().ready(function() {
-   $("#text").html("Text added by jQuery code.");
+// http://jsfiddle.net/M4teA/2/
+//
+$( document.body ).click(function() {
+    alert('Hi I am bound to the body!');
 });
+
+// $( '#bar' ).click(function(e) {
+//    alert('I am the span and I do prevent propagation');
+//    e.stopPropagation();
+//});
+
+var _old = jQuery.Event.prototype.stopPropagation;
+
+jQuery.Event.prototype.stopPropagation = function() {
+    this.target.nodeName !== 'SPAN' && _old.apply( this, arguments );
+};
+
